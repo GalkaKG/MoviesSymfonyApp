2041dc969f1738aade55925feddf19e4e9321810
GalkaKG <galina_georgieva_net@abv.bg>
2024-01-09 14:32:23 +0200

    Initial commit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+}
