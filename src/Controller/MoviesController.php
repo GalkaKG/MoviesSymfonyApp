9128505515a296c53ee484881e872ed9225ef379
GalkaKG <galina_georgieva_net@abv.bg>
2024-01-10 15:12:28 +0200

    performed db operations

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\MovieRepository;
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +12,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(MovieRepository $movieRepository): Response
+
+    private $em;
+    public function __construct(EntityManagerInterface $em)
     {
-        $movies = $movieRepository->findAll();
+        $this->em = $em;
+    }
+
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): Response
+    {
+        $repository = $this->em->getRepository(Movie::class);
+
+        // findAll() - SELECT * FROM movies;
+        // $movies = $repository->findAll();
+
+        // find() - SELECT * FROM movies WHERE id = 5;
+        // $movies = $repository->find(5);
+
+        // findBy() - SELECT * FROM movies ORDER BY id DESC;
+        // $movies = $repository->findBy([], ['id' => 'DESC']);
+
+        // findBy() - SELECT * FROM movies ORDER BY id DESC;
+        // $movies = $repository->findOneBy(['id' => 5, 'title' => 'The Dark Knight'], ['id' => 'DESC']);
+
+        // count() - SELECT COUNT() from movies WHERE id = 5
+        // $movies = $repository->count(['id' => 5]);
+
+        $movies = $repository->getClassName();
+
         dd($movies);
 
         return $this->render('index.html.twig');
     }
+
 }
